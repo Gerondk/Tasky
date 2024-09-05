77b035fbadb8031290560b50545148fd84eb736e
Gerond Kwenda <gerond@chargeAtFriends.com>
2024-09-05 23:09:15 +0200

    Implement splashscreen

--- app/src/main/java/com/gkp/tasky/MainActivity.kt
+++ app/src/main/java/com/gkp/tasky/MainActivity.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gkp.core.designsystem.theme.TaskyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
+
         setContent {
             TaskyTheme {
                 Column(
