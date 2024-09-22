8eab25cd7c48ba6d744994c7530c88fa940a8c81
Gerond Kwenda <gerond@chargeAtFriends.com>
2024-09-22 18:10:13 +0200

    Implement the refresh token mechanism

---
+++ core/network/src/main/java/com/gkp/core/network/AccessTokenRefreshHandler.kt
@@ -0,0 +1,41 @@
+package com.gkp.core.network
+
+import com.gkp.auth.domain.session.SessionStorage
+import com.gkp.core.domain.util.TaskyResult
+import com.gkp.core.network.model.RefreshTokenBody
+import com.gkp.core.network.util.networkApiCall
+import kotlinx.coroutines.flow.Flow
+import okhttp3.Response
+import java.net.HttpURLConnection.HTTP_UNAUTHORIZED
+
+class AccessTokenRefreshHandler(
+    private val sessionStorage: SessionStorage,
+    private val taskyRetrofitApi: TaskyRetrofitApi,
+) {
+
+    fun refreshAccessToken(): Flow<TaskyResult<Unit>> {
+       return networkApiCall {
+            val authInfo = sessionStorage.getAuthInfo()
+            val refreshTokenResponse = taskyRetrofitApi.refreshToken(
+                refreshTokenBody = RefreshTokenBody(
+                    refreshToken = authInfo.refreshToken,
+                    userId = authInfo.userId
+                )
+            )
+            sessionStorage.saveAuthInfo(
+                authInfo.copy(
+                    accessToken = refreshTokenResponse.accessToken,
+                    accessTokenExpirationTimestamp = refreshTokenResponse.accessTokenExpirationTimestamp
+                )
+            )
+        }
+    }
+
+    fun shouldRefreshAccessToken(response: Response): Boolean {
+        val authUrlPathSegments = listOf("login", "register")
+        return with(response) {
+            code == HTTP_UNAUTHORIZED &&
+                    request.url.pathSegments.intersect(authUrlPathSegments).isEmpty()
+        }
+    }
+}
--- core/network/src/main/java/com/gkp/core/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/gkp/core/network/di/NetworkModule.kt
@@ -1,15 +1,20 @@
 package com.gkp.core.network.di
 
-import com.gkp.network.BuildConfig
-import com.gkp.core.network.TaskyApiInterceptor
+import com.gkp.core.network.AccessTokenRefreshHandler
+import com.gkp.core.network.interceptor.TaskyApiInterceptor
 import com.gkp.core.network.TaskyRetrofitApi
+import com.gkp.core.network.interceptor.TaskyRefreshTokenInterceptor
+import com.gkp.network.BuildConfig
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.qualifier.qualifier
 import org.koin.dsl.module
 import retrofit2.Retrofit
+
+const private val TASKY_API_REFRESH_TOKEN = "TASKY_API_REFRESH_TOKEN"
 
 val networkModule = module {
     single {
@@ -20,7 +25,7 @@ val networkModule = module {
         }
         OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
-            .addInterceptor(TaskyApiInterceptor())
+            .addInterceptor(TaskyApiInterceptor(get()))
             .build()
     }
 
@@ -32,4 +37,31 @@ val networkModule = module {
             .build()
             .create(TaskyRetrofitApi::class.java)
     }
+
+    single(qualifier = qualifier(TASKY_API_REFRESH_TOKEN)) {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            if (BuildConfig.DEBUG) {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+        }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(TaskyRefreshTokenInterceptor())
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(client)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+            .create(TaskyRetrofitApi::class.java)
+
+    }
+    single {
+        AccessTokenRefreshHandler(
+            get(),
+            get(qualifier = qualifier(TASKY_API_REFRESH_TOKEN)),
+        )
+    }
 }
+
